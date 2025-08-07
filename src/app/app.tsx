a20706da4cc601fb800238302c8f399535888398
maxim <trifonov.max@mail.ru>
2025-08-07 15:39:54 +0300

    feat: add lazy loading

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,22 @@
-import {Map} from '../features/map';
-import {VehicleManager} from '../features/vehicle-manager';
+import React from 'react';
 
 import './app.scss';
+
+const LazyMap = React.lazy(() => import('../features/map'));
+const LazyVehicleManager = React.lazy(
+  () => import('../features/vehicle-manager')
+);
 
 export const App = () => {
   return (
     <div className="app">
-      <VehicleManager />
-      <Map />
+      <React.Suspense fallback={<div>VehicleManager LOAD!!!</div>}>
+        <LazyVehicleManager />
+      </React.Suspense>
+
+      <React.Suspense fallback={<div>MAP LOAD!!!</div>}>
+        <LazyMap />
+      </React.Suspense>
     </div>
   );
 };
--- src/features/vehicle-manager/index.ts
+++ src/features/vehicle-manager/index.ts
@@ -1,1 +1,1 @@
-export {VehicleManager} from './index.tsx';
+export {default} from './index.tsx';
